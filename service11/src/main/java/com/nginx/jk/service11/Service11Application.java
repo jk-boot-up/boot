4113dd954518b5314c8f9eef4202266db55fcb97
jayasekher <noreply@dummy.noreply.com>
2019-12-27 00:41:51 +0530

    Initial draft to support nginx upstream configuration

---
+++ service11/src/main/java/com/nginx/jk/service11/Service11Application.java
@@ -0,0 +1,13 @@
+package com.nginx.jk.service11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Service11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Service11Application.class, args);
+	}
+
+}
---
+++ service11/src/test/java/com/nginx/jk/service11/Service11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nginx.jk.service11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Service11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
