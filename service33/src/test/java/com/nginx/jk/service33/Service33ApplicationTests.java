27a872fd4c61d7a0ac006e91328d8259bcf073eb
jayasekher <noreply@dummy.noreply.com>
2019-12-27 13:01:22 +0530

    Revert "Revert "Initial draft service-33 to support nginx upstream config testing""
    This reverts commit dd73c233

---
+++ service33/src/test/java/com/nginx/jk/service33/Service33ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nginx.jk.service33;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Service33ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
