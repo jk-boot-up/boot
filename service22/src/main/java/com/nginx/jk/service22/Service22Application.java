13841798e303e65df470c18cbf87d233b956d4e0
jayasekher <noreply@dummy.noreply.com>
2019-12-27 12:55:33 +0530

    Initial draft service-22 to support nginx upstream testing

---
+++ service22/src/main/java/com/nginx/jk/service22/Service22Application.java
@@ -0,0 +1,13 @@
+package com.nginx.jk.service22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Service22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Service22Application.class, args);
+	}
+
+}
---
+++ service22/src/test/java/com/nginx/jk/service22/Service22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nginx.jk.service22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Service22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
