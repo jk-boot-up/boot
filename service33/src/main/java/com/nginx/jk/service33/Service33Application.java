b4c22ce3447bccf6f5bd7cb78e7e0fe36172c060
jayasekher <noreply@dummy.noreply.com>
2019-12-27 12:58:39 +0530

    Initial draft service-33 to support nginx upstream config testing

---
+++ service33/src/main/java/com/nginx/jk/service33/Service33Application.java
@@ -0,0 +1,13 @@
+package com.nginx.jk.service33;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Service33Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Service33Application.class, args);
+	}
+
+}
